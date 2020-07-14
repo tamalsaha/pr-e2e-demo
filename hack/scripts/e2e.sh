62908dedf3c2cb9932b7cd4c9f18b016d44f85b1
Tamal Saha <tamal@appscode.com>
2020-07-14 04:55:17 -0700

    added-all

--- hack/scripts/e2e.sh
+++ hack/scripts/e2e.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 url="/repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER/reviews"
-
-result=$(hub api $url | jq --raw-output '[.[].state] | contains(["APPROVED"])')
+pr_approved=$(hub api $url | jq --raw-output '[.[].state] | contains(["APPROVED"])')
+[$pr_approved == "true"] || { echo "pr $PR_NUMBER is not approved!"; exit 1; }
 
 if [ $result == "false" ]; then
 	exit 1;
